9546be696000e374f2757bcf56f8f216ec7daeb4
Jackson Stone <jackson.stone@kuali.co>
2019-05-02 08:15:46 -0400

    Reels through side  preview

--- client/js/ui/page-content/deck/components/card-edit-stack.js
+++ client/js/ui/page-content/deck/components/card-edit-stack.js
@@ -1,5 +1,22 @@
 const { html } = require('lit-html/lit-html')
+const numberOfVisibleCards = 6
+const getVisibleCards = (currentCardId, cards) => {
+  if (!cards || !currentCardId) return []
+  const cardLength = cards.length
+  if (cardLength < numberOfVisibleCards) return cards
+  let currentIndex
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].id === currentCardId) {
+      currentIndex = i
+      break
+    }
+  }
 
-module.exports = (currentCard, cards) => {
-  return html`${currentCard} : ${JSON.stringify(cards)}`
+  if (cardLength - (currentIndex + numberOfVisibleCards) > 0) {
+    return cards.slice(currentIndex, currentIndex + numberOfVisibleCards)
+  }
+  return cards.slice(cards.length - numberOfVisibleCards)
+}
+module.exports = (currentCardId, cards) => {
+  return html`${JSON.stringify(getVisibleCards(currentCardId, cards))}`
 }
