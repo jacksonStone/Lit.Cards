044a8ae89566016d0a0515eceda522cf65c9f2a4
Jackson Stone <jacksonastone@gmail.com>
2020-01-13 07:39:16 -0500

    Update env checker as well

--- server/validate-envs.js
+++ server/validate-envs.js
@@ -4,16 +4,15 @@ const listOfNeededEnvs = [
   'SIMPLE_NOTE_HMAC_KEY',
   'EMAIL_ADDRESS',
   'SENDGRID_API_KEY',
-  'AWS_ACCESS_KEY',
-  'AWS_SECRET_ACCESS_KEY',
+  // 'AWS_ACCESS_KEY',
+  // 'AWS_SECRET_ACCESS_KEY',
   'STRIPE_PUBLIC_KEY',
   'STRIPE_PRIVATE_KEY',
   'STRIPE_WEBHOOK_SECRET',
   'SITE_DOMAIN_ROOT',
   'NODE_ENV',
-  'AWS_ACCESS_KEY',
-  'AWS_SECRET_ACCESS_KEY',
-  'DEPLOYMENT_KEY'
+  'MONGO_CONNECTION_STRING',
+  'MONGO_DATABASE_NAME'
 ];
 for(let i of listOfNeededEnvs) {
   if(!envs[i]) throw new Error("Missing environment variable: " + i);
