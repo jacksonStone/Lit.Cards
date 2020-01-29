c305a97344e8353f6f329ddce948acd5a498e27d
Jackson Stone <jacksonastone@gmail.com>
2020-01-28 23:36:16 -0500

    Try to account for jpeg orientation

--- client/browser-abstractions/file-upload.js
+++ client/browser-abstractions/file-upload.js
@@ -1,10 +1,58 @@
 function getFileFromFileUploadEvent (e) {
   return e.currentTarget.files[0]
 }
+// //https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side/32490603#32490603
 
+function getOrientation(file, callback) {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+
+      var view = new DataView(e.target.result);
+      if (view.getUint16(0, false) != 0xFFD8)
+      {
+          return callback(-2);
+      }
+      var length = view.byteLength, offset = 2;
+      while (offset < length) 
+      {
+          if (view.getUint16(offset+2, false) <= 8) return callback(-1);
+          var marker = view.getUint16(offset, false);
+          offset += 2;
+          if (marker == 0xFFE1) 
+          {
+              if (view.getUint32(offset += 2, false) != 0x45786966) 
+              {
+                  return callback(-1);
+              }
+
+              var little = view.getUint16(offset += 6, false) == 0x4949;
+              offset += view.getUint32(offset + 4, little);
+              var tags = view.getUint16(offset, little);
+              offset += 2;
+              for (var i = 0; i < tags; i++)
+              {
+                  if (view.getUint16(offset + (i * 12), little) == 0x0112)
+                  {
+                      return callback(view.getUint16(offset + (i * 12) + 8, little));
+                  }
+              }
+          }
+          else if ((marker & 0xFF00) != 0xFF00)
+          {
+              break;
+          }
+          else
+          { 
+              offset += view.getUint16(offset, false);
+          }
+      }
+      return callback(-1);
+  };
+  reader.readAsArrayBuffer(file);
+}
 function getFileData (e) {
-  return new Promise((resolve, reject) => {
-    let file = getFileFromFileUploadEvent(e)
+  let file = getFileFromFileUploadEvent(e)
+  return Promise.all([new Promise((resolve, reject) => {
     let reader = new window.FileReader()
     let timeoutId = window.setTimeout(() => {
       // TODO::Try to handle this in a way that gets surfaced to the user
@@ -16,10 +64,15 @@ function getFileData (e) {
       resolve(e.target.result)
     }
     reader.readAsDataURL(file)
-  })
+  }), new Promise(resolve => {
+      //For weird iphone selfy stuff
+      getOrientation(file, resolve);
+    })
+  ]);
 }
-
-async function getImageAtDifferentSize (imageData, ...sizes) {
+//https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side/32490603#32490603
+async function getImageAtDifferentSize (imageDataAndOrientation, ...sizes) {
+  let [imageData, orientation] = imageDataAndOrientation;
   let defaultSize = 600
   sizes = !sizes.length ? [[defaultSize, defaultSize]] : sizes
   return new Promise((resolve) => {
@@ -45,9 +98,32 @@ async function getImageAtDifferentSize (imageData, ...sizes) {
             height = MAX_HEIGHT
           }
         }
-        canvasElement.width = width
-        canvasElement.height = height
-        ctx.drawImage(img, 0, 0, width, height)
+
+        // set proper canvas dimensions before transform & export
+        if (4 < orientation && orientation < 9) {
+          canvasElement.width = height;
+          canvasElement.height = width;
+        } else {
+          canvasElement.width = width;
+          canvasElement.height = height;
+        }
+
+        // transform context before drawing image
+        switch (orientation) {
+          case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+          case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+          case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+          case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+          case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+          case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+          case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+          default: break;
+        }
+
+        // draw image
+        ctx.drawImage(img, 0, 0, width, height);
+
+          // rotateImageBasedOnOrientation(7, ctx, width, height);
         results.push(canvasElement.toDataURL('image/jpeg', 0.9))
       }
       resolve(results)
